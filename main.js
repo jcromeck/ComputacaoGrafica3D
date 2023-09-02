320caf5d249d3aea15a6b34ddde456f8e641eeb1
João Henrique Moreira01~ <jcromeck@gmail.com>
2023-09-02 02:21:23 -0300

    Textura + Materiais

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color( 'white' );
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
+const texture = new THREE.TextureLoader().load('textures/textura.png' );
 
 var input = document.querySelector("#geometry");
 const btn = document.getElementById("btn");
@@ -24,13 +26,27 @@ btn.addEventListener("click", function(){
         scene.remove(obj); 
    }
     switch(input.value){
-        case 'Capsula'://Capsula
+        case 'Capsula':
             geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
             break;
-        case 'Cubo'://Cubo
+        case 'Cubo':
             geometry = new THREE.BoxGeometry( 1, 1, 1 );
             break;
+        case 'Esfera':
+            geometry = new THREE.SphereGeometry( 1, 32, 16 ); 
+            break;
+        case 'Linha basica':
+            material = new THREE.LineBasicMaterial( { color: 0x00ff00});
+            break;
+        case 'Meshdepth':
+            material = new THREE.MeshDepthMaterial( { wireframe: true } );
+            break;
+        case 'LoadTextura':
+            material = material = new THREE.MeshBasicMaterial( { map:texture } );
+            break;
         default:
+            geometry = new THREE.TorusKnotGeometry( 1, 0.18, 100, 10 );
+            material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
             break;
     }
     objeto = new THREE.Mesh( geometry, material );
